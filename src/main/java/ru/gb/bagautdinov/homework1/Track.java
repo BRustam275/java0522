e44bc9a9187e9b011152a0b4e1c43a62b49ce22f
Rustam <r.bagautdinov275@gmail.com>
2022-05-30 16:37:23 +0500

    Task1

---
+++ src/main/java/ru/gb/bagautdinov/homework1/Track.java
@@ -0,0 +1,17 @@
+package ru.gb.bagautdinov.homework1;
+
+public class Track {
+    private int distance;
+
+    public Track(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean doRun(Object object) {
+        if (object instanceof Runnable) {
+            return ((Runnable) object).run(distance);
+        }
+        return false;
+    }
+
+}
